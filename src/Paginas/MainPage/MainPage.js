7996cebe8b0dcbcec3749654b41ada8e77ee8f10
RodoDSHub <rodod.suarez@gmail.com>
2023-09-01 15:13:26 -0300

    Cambié el color de las tarjetas para que las completadas se pongan grises y las pendientes sean las verdes.

--- src/Paginas/MainPage/MainPage.js
+++ src/Paginas/MainPage/MainPage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './MainPage.css'
-import Button from '../../componentes/button/button'
 import TodoList from '../../componentes/toDO/todo'
 import { useState } from 'react'
 import data from './data.json'
@@ -8,18 +7,18 @@ import data from './data.json'
 
 function MainPage() {
 
-  const[ todos, setTodos] = useState(data)
+  const [todos, setTodos] = useState(data)
 
   const onComplete = (id) => {
-      console.log("task", id);
-      
-      setTodos(todos.map((todo)=>{
-        return todo.id == Number(id) ? {...todo, completed : !todo.completed} : {...todo}
-      }))
+    console.log("task", id);
+
+    setTodos(todos.map((todo) => {
+      return todo.id === Number(id) ? { ...todo, completed: !todo.completed } : { ...todo }
+    }))
   }
 
   const onDeleteTask = (id) => {
-    setTodos([...todos].filter(todo => todo.id != id ))
+    setTodos([...todos].filter(todo => todo.id !== id))
   }
 
   return (
@@ -28,18 +27,19 @@ function MainPage() {
     <div className='mainpage1'>
       <header>
         <img
-        className='imagen'
-        src='./8.png'
-        width={100}
-        height={100}
+          className='imagen'
+          src='./8.png'
+          alt='8'
+          width={100}
+          height={100}
         >
         </img>
         <h1 className='maintittle'>!Bienvenido a la pagina del grupo 8!</h1>
       </header>
 
       <div className='taskTable'>
-       <h2>Lista de tareas</h2>
-       <TodoList todos={todos} onComplete={onComplete} onDeleteTask={onDeleteTask}/>
+        <h2>Lista de tareas</h2>
+        <TodoList todos={todos} onComplete={onComplete} onDeleteTask={onDeleteTask} />
 
       </div>
     </div>
--- src/componentes/toDO/TodoItem.js
+++ src/componentes/toDO/TodoItem.js
@@ -1,33 +1,33 @@
 import React from 'react'
 import './todoItem.css'
 
-function TodoItem({todo, onComplete, onDeleteTask}) {
-    
-    const getStyle = () =>{
-        return{
-            padding: "15px",
-            margin:"20px",
-            fontSize: todo.completed ? "larger" : "larger",
-            fontFamily: "Verdana, Geneva, Tahoma, sans-serif",
-            backgroundColor: todo.completed ? "rgb(27, 232, 51)" : "grey",
-            borderRadius: "10px",
-        }
-    }
-  
-    return (
-     <div style={getStyle()}>
-        <input type='checkbox' checked={todo.completed} 
-          onChange={ () => onComplete(todo.id)}
-          className='taskCheckbox'
-        />
-        {todo.task}
-        <button className='DeleteButton' onClick={()  => onDeleteTask(todo.id)} >X</button>
-       <div className='description-div'>
-       {todo.description}
-       </div> 
-       <button className='edit-button' >Editar</button>
+function TodoItem({ todo, onComplete, onDeleteTask }) {
 
-     </div>
+  const getStyle = () => {
+    return {
+      padding: "15px",
+      margin: "20px",
+      fontSize: todo.completed ? "larger" : "larger",
+      fontFamily: "Verdana, Geneva, Tahoma, sans-serif",
+      backgroundColor: todo.completed ? "grey" : "rgb(27, 232, 51)",
+      borderRadius: "10px",
+    }
+  }
+
+  return (
+    <div style={getStyle()}>
+      <input type='checkbox' checked={todo.completed}
+        onChange={() => onComplete(todo.id)}
+        className='taskCheckbox'
+      />
+      {todo.task}
+      <button className='DeleteButton' onClick={() => onDeleteTask(todo.id)} >X</button>
+      <div className='description-div'>
+        {todo.description}
+      </div>
+      <button className='edit-button' >Editar</button>
+
+    </div>
 
   )
 }
